55a764b3c309a295ec9bdacbf9b68830b0477d58
Jesse Thomason <jesse@cs.utexas.edu>
2018-01-15 10:46:40 -0600

    prepped for ablation batch 3 part 1

--- www/generate_code.php
+++ www/generate_code.php
@@ -44,7 +44,7 @@ else {
 
   # Show exit instructions.
   $uid = $_POST['uid'];
-  $mturk_code = $uid."_".substr(sha1("phm_salted_hash".$uid."rwhpidcwha_test3np2_2"),0,13);
+  $mturk_code = $uid."_".substr(sha1("phm_salted_hash".$uid."rwhpidcwha_test3np3_1"),0,13);
   ?>
   <div class="row">
     <div class="col-md-12">
